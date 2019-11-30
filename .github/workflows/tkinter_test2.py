052458d37a6aad78f1c2ab9eb9d80feaf91f2d1e
Zheng <57377255+forealone@users.noreply.github.com>
2019-11-30 18:30:37 +0800

    Add files via upload

--- .github/workflows/tkinter_test2.py
+++ .github/workflows/tkinter_test2.py
@@ -15,14 +15,27 @@ window.title('My Window')
 
 window.geometry('800x600')
 
+#创建主frame到主window窗口
+frame = tk.Frame(window)
+frame.pack()
+
+#创建第二层frame，到主frame上
+frame_l = tk.Frame(frame)
+frame_r = tk.Frame(frame)
+frame_l.pack(side='left')
+frame_r.pack(side='right')
+
 img = Image.open('King.jpg')
 
 photo = ImageTk.PhotoImage(img)
 
-canvas = tk.Canvas(window, bg='green', height=192, width=139)
+b1_canvas = tk.Canvas(frame_l, bg='green', height=192, width=139)
+b2_canvas = tk.Canvas(frame_r, bg='green', height=192, width=139)
 
-image = canvas.create_image(142, 0, anchor='ne', image=photo)  # 图片锚定点（图片顶端的中间点位置）放在画布（250,0）坐标处
+image = b1_canvas.create_image(142, 0, anchor='ne', image=photo)  # 图片锚定点（图片顶端的中间点位置）放在画布（250,0）坐标处
+image = b2_canvas.create_image(142, 0, anchor='ne', image=photo)  # 图片锚定点（图片顶端的中间点位置）放在画布（250,0）坐标处
 
-canvas.pack()
+b1_canvas.pack()
+b2_canvas.pack()
 
 window.mainloop()
